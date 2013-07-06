b7bfe0faf6044d377686ecbe5a9861a531198b8e
Tanawat Pitchayapryt <pwattjaidee@windowslive.com>
2013-07-06 16:06:17 +0000

    Debug web.js

--- web.js
+++ web.js
@@ -1,18 +1,19 @@
 var express = require('express');
 var fs = require('fs');
 var buf = new Buffer(256);
+var len;
 
 fs.readFile('index.html', "utf-8", function (err, data) {
   if (err) throw err;
   console.log(data);
-  buf.write(data, "utf-8");
+  len = buf.write(data, "utf-8");
 });
 
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf-8'))
+  response.send(buf.toString('utf-8', 0, len))
 });
 
 var port = process.env.PORT || 5000;
